193c0af6c99c23ae0a95a346841cc1f29f9f2aca
Unknown <ro.wemmers@gmail.com>
2018-09-20 11:08:01 +0200

    Added Model relations

--- app/Project.php
+++ app/Project.php
@@ -9,4 +9,8 @@ class Project extends Model
     public function user() {
         return $this->belongsToMany('App\User', 'participants');
     }
+
+    public function category() {
+        return $this->hasMany('App\Category', 'category');
+    }
 }
