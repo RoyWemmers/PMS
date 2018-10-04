b78b3c3e228416dbd6eb4f8ec9417edec5a172e6
Unknown <ro.wemmers@gmail.com>
2018-10-04 08:55:35 +0200

    Created logbook add and show func

--- resources/views/elements/sidebar.blade.php
+++ resources/views/elements/sidebar.blade.php
@@ -2,12 +2,11 @@
     <div id="sidebar">
         <ul>
             <li @if(str_contains(Request::url(), 'dashboard')) class="active" @endif><a href="/" >Dashboard</a></li>
+            <li @if(str_contains(Request::url(), 'logbook')) class="active" @endif><a href="/logbook">Logbook</a></li>
             <li @if(str_contains(Request::url(), 'projects')) class="active" @endif><a href="/projects" >Projects</a></li>
             <li><a href="">Users</a></li>
             <li><a href="">Roles</a></li>
-            <li><a href="">Categories</a></li>
             <li><a href="">Customers</a></li>
-            <li><a href="">Logbook</a></li>
         </ul>
     </div>
 @endsection
